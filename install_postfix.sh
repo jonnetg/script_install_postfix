eda149d60872d3da5a7b9e420c50321f46b20355
root <jonnetg@gmail.com>
2019-05-13 22:23:37 -0300

    modified:   install_postfix.sh 	deleted:    main.cf 	deleted:    opendkim.conf 	deleted:    opendmarc.conf

--- install_postfix.sh
+++ install_postfix.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 ## Script to create mailserver
 
-domain=$DOMAIN
-ip=$IP
-network=$NETWORK
-hostname=$HOSTNAME
 
 # Getting informations about server and network
-#echo -e "Type your Domain (Ex.mydomain.com):  "; read $DOMAIN
-#echo -e "Type your IP (Ex.10.10.10.20): "; read $IP
-#echo -e "Type your NETWORK (Ex.10.0.0.0/16):  "; read $NETWORK
-echo -e "Type your HOSTNAME (Ex.myserver.mydomain.com):  "; read $HOSTNAME
+echo -e "Type your Domain (Ex.mydomain.com):  "; DOMAIN= read DOMAIN
+echo -e "Type your IP (Ex.10.10.10.20): "; IP= read IP
+echo -e "Type your NETWORK (Ex.10.0.0.0/16):  "; NETWORK= read NETWORK
+echo -e "Type your HOSTNAME (Ex.myserver.mydomain.com):  "; HOSTNAME= read HOSTNAME
 
-echo $DOMAIN $IP $NETWORK
 
 ## Remove native version postfix
 echo 'Removing old versions postfix'
@@ -36,18 +31,21 @@ echo 'Configuring Postfix paramters...'
 #sudo mv /etc/postfix/main.cf /etc/postfix/main.cf.bkp
 #sudo cp ./main.cf /etc/postfix/main.cf
 
-printf "%s\n" "
+echo $DOMAIN $IP $NETWORK $HOSTNAME
+
+
+printf "%s\n"  ' 
 # Main config
 queue_directory = /var/spool/postfix
 mail_owner = postfix
 command_directory = /usr/sbin
 daemon_directory = /usr/libexec/postfix
 data_directory = /var/lib/postfix
-myhostname = $MYHOSTNAME
+myhostname = '$HOSTNAME' 
 myorigin = $mydomain
-inet_interfaces = $IP
+inet_interfaces = '$IP'
 unknown_local_recipient_reject_code = 550
-mynetworks = 127.0.0.0/8 $IP/32 $NETWORK
+mynetworks = 127.0.0.0/8 '$IP'/32 '$NETWORK'
 alias_maps = hash:/etc/aliases
 alias_database = hash:/etc/aliases
 debug_peer_level = 2
@@ -61,7 +59,6 @@ setgid_group = postdrop
 html_directory = no
 inet_protocols = all
 mydestination = $myhostname, localhost.$mydomain
-#mydestination = $myhostname, localhost.$mydomain, $mydomain
 
 #Configuration POSTIFIX AND CERTIFICATES 
 smtp_sasl_auth_enable = yes
@@ -100,7 +97,7 @@ milter_protocol = 6
 milter_default_action = accept
 smtpd_milters = inet:localhost:8891, init:localhost:8893
 
-" > /etc/postfix/main.cf 
+'> /etc/postfix/main.cf 
 
 echo 'Configuring certificates using Openssl...'
 #sudo mkdir -p /data/postfix/cert/
@@ -116,6 +113,69 @@ echo 'Configuring OpenDkim...'
 #sudo  cp /etc/opendkim.conf /etc/opendkim.conf.bkp 
 #sudo cp ./opendkim.conf /etc/
 
+printf "%s\n" '
+PidFile    /var/run/opendkim/opendkim.pid
+Mode    sv
+Syslog    yes
+SyslogSuccess    yes
+LogWhy    yes
+UserID    opendkim:opendkim
+Socket    inet:8891@localhost
+Umask    002
+Canonicalization    relaxed/relaxed
+Selector    default
+MinimumKeyBits 1024
+KeyTable    refile:/etc/opendkim/KeyTable
+SigningTable    refile:/etc/opendkim/SigningTable
+ExternalIgnoreList    refile:/etc/opendkim/TrustedHosts
+InternalHosts    refile:/etc/opendkim/TrustedHosts
+' > /etc/opendkim.conf
+
+
+printf "%s\n" '
+127.0.0.1
+::1
+'$NETWORK'
+'$IP'/32
+'$HOSTNAME'
+'$DOMAIN'
+' > /etc/opendkim/TrustedHosts
+
+printf "%s\n" '
+default._domainkey.'$DOMAIN' '$DOMAIN':default:/etc/opendkim/keys/default.private
+' > /etc/opendkim/KeyTable 
+
+
+printf "%s\n" '
+*@'$DOMAIN' default._domainkey.'$DOMAIN'
+' > /etc/opendkim/SigningTable
+
+#Generating key opendkim
+opendkim-genkey -D  /etc/opendkim/keys/ -d $DOMAIN  -s $DOMAIN
+sudo chown -R opendkim  /etc/opendkim/keys/
+
+
 echo 'Configuring OpenDmarc...'
 #sudo  cp /etc/opendmarc.conf /etc/opendmarc.conf.bkp
+#sudo  cp /etc/ignore.hosts  /etc/ignore.hosts.bkp
+
+printf "%s\n" '
+AuthservID ip-'$IP'
+Socket inet:8893@localhost
+SoftwareHeader true
+SPFIgnoreResults true
+SPFSelfValidate true
+Syslog true
+UMask 007
+UserID opendmarc:mail
+' > /etc/opendmarc.conf
+
+printf "%s\n" '
+localhost
+127.0.0.0/8
+'$NETWORK'
+' > /etc/opendmarc/ignore.hosts
+
+# STARTING SERVICES.
+#systemctl start opendmarc opendkim.service postfix 
 
